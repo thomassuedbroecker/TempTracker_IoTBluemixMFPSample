bb98c63a7c22bc159283cf3efde75210a9055ad3
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2016-01-28 12:32:07 +0100

    combine tempTracker, tempTrackerServer, tempTrackerNodeRed, tempTrackerCloudantConfiguration and tempTrackerDocumentation

--- tempTracker/platforms/android/assets/www/default/worklight/checksum.js
+++ tempTracker/platforms/android/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":1607416367,"date":1453976106412,"machine":"IBMs-MBP.fritz.box"}
-/* Date: Thu Jan 28 2016 11:15:06 GMT+0100 (CET) */
+var WL_CHECKSUM = {"checksum":1607416367,"date":1453979056028,"machine":"IBMs-MBP.fritz.box"}
+/* Date: Thu Jan 28 2016 12:04:16 GMT+0100 (CET) */
