5c2bdd0b267660c3d74ab6ca3f9015a376a8fefd
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2016-02-04 18:59:10 +0100

    combine tempTracker, tempTrackerServer, tempTrackerNodeRed, tempTrackerCloudantConfiguration and tempTrackerDocumentation

--- tempTrackerNodeRedImplementations/functions/Check_Temperature_and_create_JSON_Data.js
+++ tempTrackerNodeRedImplementations/functions/Check_Temperature_and_create_JSON_Data.js
@@ -1,10 +1,10 @@
 if ( msg !== null)
-{ // if msg !0 null begin
-  // Create data object
-  data = new Object();
+{   // if msg !0 null begin
+    // Create data object
+    data = new Object();
 
-  // *****************
-  // Get Data and time
+    // *****************
+    // Get Data and time
     var currentdate = new Date();
     var the_minutes     = "00";
     var the_seconds     = "00";
@@ -26,12 +26,12 @@ if ( msg !== null)
     data.image  = "undefined";
     data.imageLocalURI = "undefined";
 
-  // *****************
-  // Get the concrete Sensor data
-  // the input can be different based on your device/app type:
-  // "Apple, Android, Simulator do have different datastructure in the payload"
+    // *****************
+    // Get the concrete Sensor data
+    // the input can be different based on your device/app type:
+    // "Apple, Android, Simulator do have different datastructure in the payload"
     //-----------------------------------------------------------------------
-    data.temp = "20.0000"; // default
+    data.temp = 20; // default
     if (msg.payload.d.AmbTemp !== undefined) {
         data.temp     = msg.payload.d.AmbTemp;      // Typical Apple datastructure
     }
@@ -43,8 +43,9 @@ if ( msg !== null)
     if (msg.payload.d.temp !== undefined) {
         data.temp     = msg.payload.d.temp;          // Typical Simulator datastructure
     }
-    //--------------------------------------------------------------------------
-    data.iptemp = "20.0000"; //default
+
+    //-------------------------IPTEMP-------------------------------------------------
+    data.iptemp = 20; //default
     if (msg.payload.d.IRTemp !== undefined) {
         data.irtemp   = msg.payload.d.IRTemp;        // Typical Apple datastructure
     }
@@ -56,7 +57,8 @@ if ( msg !== null)
     if (msg.payload.d.objectTemp !== undefined) {
         data.irtemp   = msg.payload.d.objectTemp;    // Typical Simulator datastructure
     }
-    //--------------------------------------------------------------------------
+
+    //-------------------------OPTICAL---------------------------------------------
     data.optical = 0; // default if not defined by others
 
     if (msg.payload.d.optical !== undefined) {
@@ -66,73 +68,88 @@ if ( msg !== null)
     if (msg.payload.d.light !== undefined) {
         data.optical  = msg.payload.d.light;      // Typical Android datastructure
     }
+
     //--------------------------------------------------------------------------
     data.deviceId = msg.deviceId; // Typical datastructure for all
 
-  // *****************
-  // Add your additional sensor data
+    // *****************
+    // Add your additional sensor data
     data.sensorImage = "img/sensor-detail.png";
-    data.comment = "Currently on comment";
-    data.sensorType = "Simplelink SensorTag - TI.com";
-
+    data.comment = "Currently no comment";
+    if (data.sensorType !== undefined) {
+       data.sensorType = "Simplelink SensorTag - TI.com";
+    }
     data.image    = "undefined";
     data.imageLocalURI = "undefined";
 
-  // *****************
-  // Define your Device Type which ICON should be dispayed
-  // ======================================================
-  // You can get the Device ID from your TI App on your mobile device.
-  //
-  // (iPad:94128ececff1) (iPhone:08b5cbcfa326) (Samsung:c4be84722b07)
-  // http://www.w3schools.com/jsref/jsref_localecompare.asp
-  var ipad = "94128ececff1";
-  var iphone = "1247823ebf04"; // "08b5cbcfa326";
-  var samsung = "c4be84722b07";
+    // *****************
+    // Define your Device Type: Which ICON should be dispayed in the App?
+    // ==================================================================
+    // You can get the Device ID from your TI App on your mobile device.
+    //
+    // (iPad:94128ececff1) (iPhone:08b5cbcfa326) (Samsung:c4be84722b07)
+    // http://www.w3schools.com/jsref/jsref_localecompare.asp
+    var ipad = "94128ececff1";
+    var iphone = "1247823ebf04"; // "08b5cbcfa326";
+    var samsung = "c4be84722b07";
+    var iosfakeapp = "123456789";
 
-  if(data.deviceId.localeCompare("94128ececff1") == 0){
+    if(data.deviceId.localeCompare("94128ececff1") == 0){
      data.deviceType="iPad";
      data.deviceImageURI="img/ipad.png";
-  }
+    }
 
-  if(data.deviceId.localeCompare("08b5cbcfa326") == 0){
+    if(data.deviceId.localeCompare("08b5cbcfa326") == 0){
      data.deviceType="iPhone";
      data.deviceImageURI="img/iphone.png";
-  }
+    }
 
-  if(data.deviceId.localeCompare("c4be84722b07") == 0){
+    if(data.deviceId.localeCompare("123456789") == 0){
+     data.deviceType="iPhone";
+     data.deviceImageURI="img/iphone.png";
+     data.sensorType = "Fake App on iOS Device";
+    }
+
+    if(data.deviceId.localeCompare("c4be84722b07") == 0){
      data.deviceType="Samsung";
      data.deviceImageURI="img/samsung.png";
-  } else {
+    } else {
      data.deviceType="Container";
      data.deviceImageURI="img/container.png";
-  }
+    }
 
-  // *****************
-  // Ehningen 48.65891, 8.940540000000055
-  // http://www.viewphotos.org/germany/coordinates-of-Ehningen-5390.html
-  data.gtfs_latitude  = "48.6589";
-  data.gtfs_longitude = "8.940540000000055";
+   // *****************
+   // Ehningen 48.65891, 8.940540000000055
+   // http://www.viewphotos.org/germany/coordinates-of-Ehningen-5390.html
+   data.gtfs_latitude  = "48.6589";
+   data.gtfs_longitude = "8.940540000000055";
 
-  // Check Temperature
-  if ( data.temp < 25 )
-  {
+   if (data.sensorType == undefined ){
+     data.sensorType = "Simplelink SensorTag - TI.com";
+   }
+
+   // ******************
+   // Check Temperature and create message
+   // ******************
+   if ( data.temp < 25 )
+   {
      data.message= data.date + "/" + data.time + " (" + data.temp + ") within safe limits";
      data.status ="SAFE";
-  } else {
-     data.message= data.date + "/" + data.time + " (" + data.temp + ") is in critial limits";
+   } else {
+     data.message= data.date + "/" + data.time + " (" + data.temp + ") is in critical limits";
      data.status ="CRITICAL";
-  }
+   }
 
-  // *****************
-  // Build data your own datastructure
-  // -> create json root object
-  sensorroot = new Object();
-  sensorroot.sensordatavalue = data;
+   // *****************
+   // Build data your own datastructure
+   // -> create json root object
+   sensorroot = new Object();
+   sensorroot.sensordatavalue = data;
 
-  // *****************
-  // Set your data as the payload,
-  // which will be used in the next node as input.
-  msg.payload = sensorroot;
+   // *****************
+   // Set your data as the payload,
+   // which will be used in the next node as input.
+   msg.payload = sensorroot;
 } else {
     msg.payload = "No IoT Data Input";
 }// if else msg != null end
